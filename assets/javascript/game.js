e09fd3c5691b45f1a0a04374814d5bfa6f793c1a
Diego Matthews <diegomatthews9@gmail.com>
2019-06-15 20:24:34 -0700

    Created basic variables and basic index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+let score = 0;
+let correctCount = 0;
+let wrongCount = 0;
+
+let timeLeft = 0;
+let trivia = [];
+let triviaQuestions;
+let choiceBtns;
+
+let correct = $("#corrct");
+let wrong = $("#wrong");
+let timeClock = $("#time-clock");
+
+function 
