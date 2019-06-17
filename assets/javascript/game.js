2e2b10ccfba1bd53c2b555712399086797018fe0
Diego Matthews <diegomatthews9@gmail.com>
2019-06-17 12:40:44 -0700

    made question progression possible

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,70 @@
-let score = 0;
-let correctCount = 0;
-let wrongCount = 0;
 
-let timeLeft = 0;
-let trivia = [];
-let triviaQuestions;
-let choiceBtns;
+let trivia = [
+    {
+        Question: "Are you READY!!",
+        Answers: "click Start when you think you are!"
+    },
+    {
+        Question: "question 1",
+        Choices: [
+            "Answer A <br>",
+            "Answer B <br>",
+            "Answer C <br>",
+            "Answer D <br>"
+        ]
 
-let correct = $("#corrct");
+    },
+    {
+        Question: "question 2",
+        Choices: [
+            "Answer A <br>",
+            "Answer B <br>",
+            "Answer C <br>",
+            "Answer D <br>"
+        ]
+    },
+    {
+        Question: "question 3",
+        Choices: [
+            "Answer A <br>",
+            "Answer B <br>",
+            "Answer C <br>",
+            "Answer D <br>"
+        ]
+    }
+];
+console.log(trivia);
+
+let qNum = 0;
+
+let scoreBox = $(".score-box");
+let correct = $("correct");
 let wrong = $("#wrong");
 let timeClock = $("#time-clock");
+let triviaBox = $(".trivia-box");
+let question = $("#question");
+let choices = $("#choices");
+let nextBtn = $("#next-btn");
 
-function timer(){
+function writeTrivia() {
 
-}
-
-function writeTrivia(){
-
-}
-
-function scoreBox(){
-
-}
-
-function choiceBtns(){
+    if (qNum < trivia.length) {
+        question.html(trivia[qNum].Question);
+        choices.html(trivia[qNum].Choices);
+    }
 
 }
 
+function nextButton() {
+    nextBtn.on("click", function () {
+        qNum++;
+        writeTrivia();
+    });
+}
+
+function triviaInit() {
+    writeTrivia();
+    nextButton();
+}
+
+triviaInit();
