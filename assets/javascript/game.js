01161fb3fa4fdbba5df3fbbfdce20e993d8173b5
Diego Matthews <diegomatthews9@gmail.com>
2019-06-23 12:47:18 -0700

    created basic object for trivia q and a

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,123 @@
 
 let trivia = [
-    {
-        Question: "Are you READY!!",
-        Answers: "click Start when you think you are!"
-    },
-    {
-        Question: "question 1",
-        Choices: [
-            "Answer A <br>",
-            "Answer B <br>",
-            "Answer C <br>",
-            "Answer D <br>"
-        ]
 
+    {
+        Question: "Do you really know the best games in the Universe?!",
+        Choices: "Begin when you are ready."
     },
     {
-        Question: "question 2",
-        Choices: [
-            "Answer A <br>",
-            "Answer B <br>",
-            "Answer C <br>",
-            "Answer D <br>"
-        ]
+        Question: 'What is the first level of "Halo: Combat Evolved" called?',
+        Choices: {
+            a: "343 Guilty Spark",
+            b: "Sierra 117",
+            c: "The Pillar of Autumn",
+            d: "Prologue"
+        },
+        answer: "b"
     },
     {
-        Question: "question 3",
-        Choices: [
-            "Answer A <br>",
-            "Answer B <br>",
-            "Answer C <br>",
-            "Answer D <br>"
-        ]
+        Question: 'What is the Installation number of "The Ark"?',
+        Choices: {
+            a: "Installation 04",
+            b: "Installation 00",
+            c: "Installation 05",
+            d: "Installation 01"
+        },
+        answer: "b"
+    },
+    {
+        Question: 'Where is the portal to "The Ark" located?',
+        Choices: {
+            a: "Europe",
+            b: "Asia",
+            c: "America",
+            d: "Africa"
+        },
+        answer: "b"
+    },
+    {
+        Question: 'How many main types of Flood are there in "Halo 3"?',
+        Choices: {
+            a: "Two",
+            b: "Three",
+            c: "Four",
+            d: "Five"
+        },
+        answer: "b"
+    },
+    {
+        Question: 'How many main types of Flood are there in "Halo 3"?',
+        Choices: {
+            a: "Two",
+            b: "Three",
+            c: "Four",
+            d: "Five"
+        },
+        answer: "b"
+    },
+    {
+        Question: 'How many main types of Flood are there in "Halo 3"?',
+        Choices: {
+            a: "Two",
+            b: "Three",
+            c: "Four",
+            d: "Five"
+        },
+        answer: "b"
+    },
+    {
+        Question: 'How many main types of Flood are there in "Halo 3"?',
+        Choices: {
+            a: "Two",
+            b: "Three",
+            c: "Four",
+            d: "Five"
+        },
+        answer: "b"
+    },
+    {
+        Question: 'How many main types of Flood are there in "Halo 3"?',
+        Choices: {
+            a: "Two",
+            b: "Three",
+            c: "Four",
+            d: "Five"
+        },
+        answer: "b"
+    },
+    {
+        Question: 'How many main types of Flood are there in "Halo 3"?',
+        Choices: {
+            a: "Two",
+            b: "Three",
+            c: "Four",
+            d: "Five"
+        },
+        answer: "b"
+    },
+    {
+        Question: 'How many main types of Flood are there in "Halo 3"?',
+        Choices: {
+            a: "Two",
+            b: "Three",
+            c: "Four",
+            d: "Five"
+        },
+        answer: "b"
     }
 ];
 console.log(trivia);
 
 let qNum = 0;
 
-let scoreBox = $(".score-box");
-let correct = $("correct");
-let wrong = $("#wrong");
-let timeClock = $("#time-clock");
-let triviaBox = $(".trivia-box");
-let question = $("#question");
-let choices = $("#choices");
-let nextBtn = $("#next-btn");
+const scoreBox = $(".score-box");
+const correct = $("correct");
+const wrong = $("#wrong");
+const timeClock = $("#time-clock");
+const triviaBox = $(".trivia-box");
+const question = $("#question");
+const choices = $("#choices");
+const nextBtn = $("#next-btn");
 
 function writeTrivia() {
 
